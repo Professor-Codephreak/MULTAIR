bc761f936a01f2198417c766a710b5b52fe9a8b3
codephreak <codephreak@dmg.finance>
2025-02-28 23:01:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  * MULTAIR (c) 2025 Gregory L. Magnusson
  *
  * Based on original works:
- * Multer (c) 2014-2021 Hage Yaapa <yaapa@live.com>
- * Busboy (c) 2010 Benjamin Thomas
+ * multer (c) 2014-2021 Hage Yaapa <yaapa@live.com>
+ * busboy (c) 2010 Benjamin Thomas
  *
  * MIT License
  */
